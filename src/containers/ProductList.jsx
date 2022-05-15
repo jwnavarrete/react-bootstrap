d96e3589238be7491b99d4a4e8ed987e546f8ed0
jwnavarrete <jwnavarretez@gmail.com>
2022-05-15 17:58:37 -0500

    add hooks product list

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,27 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import ProductItem from '@components/ProductItem';
-import '@styles/ProductList.scss';
-import axios from 'axios';
+import React from "react";
+import ProductItem from "@components/ProductItem";
+import "@styles/ProductList.scss";
+import useGetProducts from "@hooks/useGetProducts";
 
-const API = 'http://api.escuelajs.co/api/v1/products';
+const API = "http://api.escuelajs.co/api/v1/products";
 
 const ProductList = () => {
-	const [products, setProducts] = useState([]);
+  const products = useGetProducts(API);
 
-	useEffect(async () => {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, [])
-
-	return (
-		<section className="main-container">
-			<div className="ProductList">
-				{products.map(product => (
-					<ProductItem />
-				))}
-			</div>
-		</section>
-	);
-}
+  return (
+    <section className="main-container">
+      <div className="ProductList">
+        {products.map((product) => (
+          <ProductItem product={product} key={product.id} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default ProductList;
